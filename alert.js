96c1a168990ad87e56c8fdfa2a8afbdb952d1e72
Egore <halfnext@gmail.com>
2022-02-08 14:06:34 +0300

    Test

---
+++ alert.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// let admin;
+// let name = "John"
+// admin = name;
+// alert( admin );
+// console.log(admin);
+
+// let planet = "Earth";
+// let currentUser;
+
+// let name = prompt("What is the “official” name of JavaScript?", 'ECMAScript');
+// if (name == 'ECMAScript') {
+//     alert( "Right!" );
+// }   else {
+//     alert( "wrong" );
+// }
+
+let number = prompt("Give a number");
+ (number > 0) ? alert( "1" ): (number < 0) ? alert( "-1" ): alert("0");
