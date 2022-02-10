b188ba6f8d1266a8e9d9581b8779397314d3c80d
Egore <halfnext@gmail.com>
2022-02-10 20:58:55 +0300

    Object 3

---
+++ Objects.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// let user = {};
+// user.name = "John";
+// user.surname = "Smith";
+// user.name = "Pete";
+// delete user.name;
+
+// function isEmpty (object) {
+//     for (let key in object) {
+//       alert(!key ?  "true" : "false");
+//       return;
+//     }
+// };
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  };
+
+let sum = 0 
+
+for (let key in salaries) {
+   sum += salaries[key]; 
+   console.log(salaries.key);
+}
+console.log(sum);
+
