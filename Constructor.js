c862b4265c5f8f44926d897ed45ba60a86ef6e81
Egore <halfnext@gmail.com>
2022-02-13 16:12:31 +0300

    Constructor 2

--- Constructor.js
+++ Constructor.js
@@ -1,20 +1,36 @@
 "use strict";
 
-function Calculator() {
+// function Calculator() {
+//     this.read = function() {
+//         this.a = +prompt("Enter operand a");
+//         this.b = +prompt("Enter operand b");
+//         },
+//         this.sum = function() {
+//             return this.a + this.b;
+//         },
+//         this.mul = function() {
+//             return this.a * this.b;
+//         }
+// };
+
+// let calculator = new Calculator();
+// calculator.read();
+
+// alert( "Sum=" + calculator.sum() );
+// alert( "Mul=" + calculator.mul() );
+
+function Accumulator(startingValue) {
+    this.value = startingValue;
+
     this.read = function() {
-        this.a = +prompt("Enter operand a");
-        this.b = +prompt("Enter operand b");
-        },
-        this.sum = function() {
-            return this.a + this.b;
-        },
-        this.mul = function() {
-            return this.a * this.b;
-        }
+        this.value += +prompt("Increment number")
+
+    }
 };
 
-let calculator = new Calculator();
-calculator.read();
+let accumulator = new Accumulator(1); // initial value 1
 
-alert( "Sum=" + calculator.sum() );
-alert( "Mul=" + calculator.mul() );
+accumulator.read(); // adds the user-entered value
+accumulator.read(); // adds the user-entered value
+
+alert(accumulator.value); // shows the sum of these values
