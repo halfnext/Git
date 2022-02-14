3e8da01105e0cf193338d2a52f02f09598df860c
Egore <halfnext@gmail.com>
2022-02-14 22:13:22 +0300

    num`

--- Constructor.js
+++ Constructor.js
@@ -19,18 +19,18 @@
 // alert( "Sum=" + calculator.sum() );
 // alert( "Mul=" + calculator.mul() );
 
-function Accumulator(startingValue) {
-    this.value = startingValue;
+// function Accumulator(startingValue) {
+//     this.value = startingValue;
 
-    this.read = function() {
-        this.value += +prompt("Increment number")
+//     this.read = function() {
+//         this.value += +prompt("Increment number")
 
-    }
-};
+//     }
+// };
 
-let accumulator = new Accumulator(1); // initial value 1
+// let accumulator = new Accumulator(1); // initial value 1
 
-accumulator.read(); // adds the user-entered value
-accumulator.read(); // adds the user-entered value
+// accumulator.read(); // adds the user-entered value
+// accumulator.read(); // adds the user-entered value
 
-alert(accumulator.value); // shows the sum of these values
+// alert(accumulator.value); // shows the sum of these values
