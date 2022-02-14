4ec45ac7287f260cba5a6102985b1be608a5d9a5
Egore <halfnext@gmail.com>
2022-02-14 22:20:35 +0300

    random

--- dataTypes.js
+++ dataTypes.js
@@ -1,5 +1,10 @@
 "use strict";
 
-let a = +prompt("The first number?");
-let b = +prompt("The second number?");
-alert(a + b);
+// let a = +prompt("The first number?");
+// let b = +prompt("The second number?");
+// alert(a + b);
+
+function random(min, max) {
+    alert((max-min) * Math.random() + min);
+};
+random(2, 8);
