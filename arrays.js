82c6c9464b5893a880f3139c824a349b2e80f040
Egore <halfnext@gmail.com>
2022-02-15 19:54:17 +0300

    arrays

---
+++ arrays.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// let styles = ["Jazz", "Blues"];
+// styles.push("Rock-n-Roll");
+// styles[Math.round((styles.length - 1) / 2)] = "Classics";
+// alert(styles.shift());
+// styles.unshift("Rap", "Raggie");
+
