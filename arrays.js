2c8629c7dfc983f420f93b3ae03e14c469967083
Egore <halfnext@gmail.com>
2022-02-15 20:21:54 +0300

    Array 2

--- arrays.js
+++ arrays.js
@@ -6,3 +6,20 @@
 // alert(styles.shift());
 // styles.unshift("Rap", "Raggie");
 
+function sumInput() {
+    let sumArr = [];
+    let a = 0;
+    while (!isNaN(Number(a = prompt("Enter number"))) && a !== null){
+        sumArr.push(+a)
+    };
+
+    a = 0;
+
+    for (let num of sumArr) {
+        a += num;
+    };
+
+    return a;
+};
+
+alert(sumInput())
