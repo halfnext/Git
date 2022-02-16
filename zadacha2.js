7eecb880d5b1b3139b254df2dd8946bc11095197
Egore <halfnext@gmail.com>
2022-02-16 14:05:59 +0300

    Zadach2 fix

--- zadacha2.js
+++ zadacha2.js
@@ -2,12 +2,13 @@
 
 function arrayCheckEven(arr) {
     for(let key of arr){
-        if (isNaN(key) || key % 2 != 0) return "Not Even Array";     
+        if (isNaN(key) || key % 2 != 0)  return "Not Even Array";     
     };
-    return "Even Array"
+    return arr.length > 0 ? "Even Array" : "Not Even Array"; 
 };
 
 console.log(arrayCheckEven([2, 6, 8, 10]))
 console.log(arrayCheckEven([2, 6, 8, 11]))
 console.log(arrayCheckEven([2, 6, 8, 'lol']))
 console.log(arrayCheckEven('lol'))
+console.log(arrayCheckEven([]))
