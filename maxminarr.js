f87d4fce778fb68de16503df3f4de3a3c96e7b0a
Egore <halfnext@gmail.com>
2022-02-16 17:58:39 +0300

    zadacha3

---
+++ maxminarr.js
@@ -0,0 +1,28 @@
+"use strict";
+
+function getMinMax(aArray) {
+    let min = 0;
+    let max = 0;
+    let idxMin;
+    let idxMax;
+    if (aArray.length == 0) return "Min/Max cannot be found";
+    for (let key of aArray) {    
+        if (isNaN(key)) return "Min/Max cannot be found";
+
+        key > max ? max = key : key < min ? min = key : "Min/Max cannot be found";
+    };  
+    idxMin = aArray.indexOf(min);
+    idxMax = aArray.indexOf(max);
+    let obj = {
+        [min]: idxMin,
+        [max]: idxMax,
+        };
+    return obj;    
+}
+
+alert(getMinMax([3,4,10,7,0])); // -> Min: 0 at index 4. Max: 10 at index 2;
+alert(getMinMax(["haha", 0])); // -> Min/Max cannot be found.
+alert(getMinMax([])); // -> Min/Max cannot be found.
+console.log(getMinMax([3,4,10,7,0])); // -> {0:4,10:2}
+console.log(getMinMax(["haha", 0])); // -> {}
+console.log(getMinMax([])); // -> {}
