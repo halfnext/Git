35c197cf843d8c84195d44da309c600b48e8784e
Egore <halfnext@gmail.com>
2022-02-16 18:26:42 +0300

    Zadacha3 fix

--- zadacha3.js
+++ zadacha3.js
@@ -4,9 +4,13 @@ function keyboard(key) {
     let keypad = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
                   "a", "s", "d", "f", "g", "h", "j", "k", "l",
                   "z", "x", "c", "v", "b", "n", "m"];
-    if (key.length > 1 || (keypad.indexOf(key.toLowerCase())) == -1) return "Check parametr!";
+    if (!(typeof key === 'string') || (key.length > 1) || (keypad.indexOf(key.toLowerCase())) == -1) return "Check parametr!";
     if (key.toLowerCase() == keypad[keypad.length -1]) return (keypad[0]);
     return keypad[keypad.indexOf(key.toLowerCase()) + 1];
 };
 
-// alert(keyboard(prompt("Enter latter")));
+alert(keyboard(prompt("Enter latter")));
+console.log(keyboard([]));
+console.log(keyboard(NaN));
+console.log(keyboard(1));
+console.log(keyboard({}));
