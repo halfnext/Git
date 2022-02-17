9484e641cd2cf7b8db080fc0ec6d1be39d775a1e
Egore <halfnext@gmail.com>
2022-02-17 19:12:06 +0300

    repeatString

--- arrays.js
+++ arrays.js
@@ -6,20 +6,31 @@
 // alert(styles.shift());
 // styles.unshift("Rap", "Raggie");
 
-function sumInput() {
-    let sumArr = [];
-    let a = 0;
-    while (!isNaN(Number(a = prompt("Enter number"))) && a !== null){
-        sumArr.push(+a)
+// function sumInput() {
+//     let sumArr = [];
+//     let a = 0;
+//     while (!isNaN(Number(a = prompt("Enter number"))) && a !== null){
+//         sumArr.push(+a)
+//     };
+
+//     a = 0;
+
+//     for (let num of sumArr) {
+//         a += num;
+//     };
+
+//     return a;
+// };
+
+// alert(sumInput())
+
+function repeatString(iAmount, sString ) {
+    let newString = '';
+    for (let i = 0; i < iAmount; i++) {
+        newString += sString;
     };
+    return newString;
+}
 
-    a = 0;
-
-    for (let num of sumArr) {
-        a += num;
-    };
-
-    return a;
-};
-
-alert(sumInput())
+repeatString(6, "Denis") // -> DenisDenisDenisDenisDenisDenis
+repeatString(3, " # ") // ->  #  #  # 
