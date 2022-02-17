5b44067cb2c1b5d8bc76d8f5f13e9b6eae1a749a
Egore <halfnext@gmail.com>
2022-02-17 17:56:11 +0300

    arrayMethods 2

--- arrayMethods.js
+++ arrayMethods.js
@@ -11,16 +11,28 @@
 //     return arr.join('');
 // };
 
-function camelize(str) {
-    let arr = str.split('-');
-    let result = arr.map((item, index) => {
-        if (index != 0) {
-           return item[0].toUpperCase() + item.slice(1);
-        } else return item;
-})
-    return result.join('');
+// function camelize(str) {
+//     let arr = str.split('-');
+//     let result = arr.map((item, index) => {
+//         if (index != 0) {
+//            return item[0].toUpperCase() + item.slice(1);
+//         } else return item;
+// })
+//     return result.join('');
+// };
+
+// camelize("background-color") == 'backgroundColor';
+// camelize("list-style-image") == 'listStyleImage';
+// camelize("-webkit-transition") == 'WebkitTransition';
+
+function filterRange(arr, a, b) {
+//    return arr.map(item => { 
+//        if (item >= a && item <= b) 
+//        return item;
+//     }); 
+    return arr.filter(item => (item >= a && item <= b));
 };
 
-camelize("background-color") == 'backgroundColor';
-camelize("list-style-image") == 'listStyleImage';
-camelize("-webkit-transition") == 'WebkitTransition';
+let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 1, 4);
