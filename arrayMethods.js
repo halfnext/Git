9176ddb11fcf6e91bae5589928877b814ac364be
Egore <halfnext@gmail.com>
2022-02-17 13:05:31 +0300

    Array Methods 1 variant map

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,14 +1,24 @@
 "use strict";
 
+// function camelize(str) {
+//     let arr = str.split('');
+//     for (let key of arr) {
+//         if (key == "-") {
+//             arr[(arr.indexOf(key) + 1)] = arr[(arr.indexOf(key) + 1)].toUpperCase();
+//             arr.splice(arr.indexOf(key), 1);
+//         }
+//     }
+//     return arr.join('');
+// };
+
 function camelize(str) {
-    let arr = str.split('');
-    for (let key of arr) {
-        if (key == "-") {
-            arr[(arr.indexOf(key) + 1)] = arr[(arr.indexOf(key) + 1)].toUpperCase();
-            arr.splice(arr.indexOf(key), 1);
-        }
-    }
-    return arr.join('');
+    let arr = str.split('-');
+    let result = arr.map((item, index) => {
+        if (index != 0) {
+           return item[0].toUpperCase() + item.slice(1);
+        } else return item;
+})
+    return result.join('');
 };
 
 camelize("background-color") == 'backgroundColor';
